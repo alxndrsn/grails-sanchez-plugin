a456db04c8a42cba5e6c381b1711f10953c037f3
Alex Anderson <alex@frontlinesms.com>
2013-03-22 11:36:19 +0300

    Changed email address listed so it doesn't break grails website.

--- SanchezGrailsPlugin.groovy
+++ SanchezGrailsPlugin.groovy
@@ -3,7 +3,7 @@ class SanchezGrailsPlugin {
 	def grailsVersion = '2.0 > *'
 	def title = 'Sanchez Plugin'
 	def author = 'Alex Anderson'
-	def authorEmail = 'http://www.github.com/alxndrsn'
+	def authorEmail = ''
 	def description = 'Grails plugin for sanchez.js javascript/html/GSP templates on client and server-side.'
 	def documentation = 'https://github.com/alxndrsn/grails-sanchez-plugin'
 	def license = 'APACHE'
